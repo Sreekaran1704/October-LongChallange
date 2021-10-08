44a4a32ee3bfafc738d8a39ca573a11d556abc6a
Ramasahayam Sreekaran Reddy <86601811+Sreekaran1704@users.noreply.github.com>
2021-10-08 07:02:07 +0530

    Create Mixture.py

---
+++ Mixture.py
@@ -0,0 +1,10 @@
+#Question : MIXTURE
+#Code :
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    if a>0 and b>0:
+        print("Solution")
+    elif a==0:
+        print("Liquid")
+    elif b==0:
+        print("Solid")
